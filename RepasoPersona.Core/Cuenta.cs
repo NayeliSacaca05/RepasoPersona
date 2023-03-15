bf36835ba89e4928ddc676359373ebdc57c8b0e2
NayeliSacaca05 <nayeli.sacacaet12d1@gmail.com>
2023-03-15 15:42:55 -0300

    PSR(Banco)

---
+++ RepasoPersona.Core/Cuenta.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepasoPersona.Core
+{
+    public class Cuenta
+    {
+        public double Saldo { get; private set; }
+        public int CBU { get; private set; }
+        public Cuenta() => Saldo = 0;
+
+        public Cuenta (double saldo, int cbu)
+        {
+            saldo = Saldo;
+            cbu = CBU;
+        }
+
+        public void Debitar(double monto)
+        {
+            if (monto <= 0)
+                throw new ArgumentException("El monto tiene que ser mayor a cero.");
+
+            if (monto > Saldo)
+                throw new InvalidOperationException("El monto supera al efectivo.");
+            Saldo -= monto;
+        }
+
+    }
+}
