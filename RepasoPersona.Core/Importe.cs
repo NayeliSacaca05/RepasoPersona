8f37f5492002a4befe8c28518ee9214128ec74cd
NayeliSacaca05 <nayeli.sacacaet12d1@gmail.com>
2023-03-16 15:48:29 -0300

    importe

--- RepasoPersona.Core/Importe.cs
+++ RepasoPersona.Core/Importe.cs
@@ -7,6 +7,14 @@ namespace RepasoPersona.Core
 {
     public class Importe
     {
-        
+        public double Monto { get; protected set; }
+        public double Saldo { get; private set; }
+        public Importe(double monto, double saldo)
+        {
+            Monto = monto;
+            Saldo = saldo;
+        }
+
+
     }
 }
